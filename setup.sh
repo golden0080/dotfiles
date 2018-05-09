d8efe261e741954cc8f91d5b8f2bfe6a2c1f9fa3
Guanghui Chen <gh@turingvideo.com>
2018-05-09 01:43:18 -0700

    setup.sh for new env setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# Some basic tools
+sudo apt install -y git wget
+sudo apt install -y silversearcher-ag
+
+# Download and setup spacemacs, need emacs25
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+ln -s $(HOME)/dotfiles/dot.spacemacs ~/.spacemacs
+sudo apt install -y python3-pip
+
+# Download and install `fd`
+FD_DL_URL=https://github.com/sharkdp/fd/releases/download/v7.0.0/fd-musl_7.0.0_amd64.deb
+rm /tmp/fd.deb
+wget $(FD_DL_URL) -o /tmp/fd.deb
+sudo dpkg -i /tmp/fd.deb
+
+# TODO(gh): optionally install stormssh
+sudo pip3 install stormssh
+
+# TODO(gh): install fzf, optionally
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
