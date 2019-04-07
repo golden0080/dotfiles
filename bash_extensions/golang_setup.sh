8d9c0e8c93226fe1e9859f8b69cbcc6a6b9b8173
Guanghui Chen <gh@turingvideo.com>
2019-04-06 21:54:52 -0700

    changed golang setup script

--- bash_extensions/golang_setup.sh
+++ bash_extensions/golang_setup.sh
@@ -1,4 +1,23 @@
-gcov () {
+# Go Version
+export GOVERSION=1.10
+
+export GOROOT="$HOME/go-install/$GOVERSION"
+export GOPATH="$HOME/go-projects/$GOVERSION"
+
+if [[ ! -d $GOROOT ]] || [[ ! -d $GOPATH ]]; then
+    unset GOVERSION
+    unset GOROOT
+    unset GOPATH
+    return
+fi
+
+export PATH="$GOROOT/bin/:$GOPATH/bin/:$PATH"
+
+function gopro() {
+    cd $GOPATH/src/$1
+}
+
+function gocov () {
     package_name=$1
     out_file=${2:-cov.out}
     echo "Coverage for" $package_name $out_file
