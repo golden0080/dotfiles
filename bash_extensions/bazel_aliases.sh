34607c890a1c56b271767669202707cf03558775
Guanghui Chen <guanghui@ikerobotics.com>
2019-06-06 16:24:40 -0700

    calculate nproc usable for bazel

--- bash_extensions/bazel_aliases.sh
+++ bash_extensions/bazel_aliases.sh
@@ -1,4 +1,8 @@
-alias bb="bazel build -c opt --config cuda --jobs $(nproc) $1"
+_NPROC=$(nproc)
+BAZEL_NPROC=`expr $_NPROC - 3`
+unset _NPROC
+
+alias bb="bazel build -c opt --config cuda --jobs $BAZEL_NPROC $1"
 alias bb_basics="bb //onboard/comms:registry_runner //onboard/nuro_viewer:nuro_viewer_offboard //playback/playback_core:playback //onboard/comms:topic_echo"
-alias bt="bazel test -c opt --config cuda --jobs $(nproc) $1"
-alias br="bazel run -c opt --config cuda --jobs $(nproc) $1"
+alias bt="bazel test -c opt --config cuda --jobs $BAZEL_NPROC $1"
+alias br="bazel run -c opt --config cuda --jobs $BAZEL_NPROC $1"
